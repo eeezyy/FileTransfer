b3ee3596c2241cc0da651f07fa583d5a48339204
Alexander Kumbeiz <eeezyy@gmail.com>
2011-01-06 14:12:01 +0000

    formatted code
    git-svn-id: file:///home/node/svn/hts@11 cfcc7383-0204-4295-856a-6b29432e848c

--- server.c
+++ server.c
@@ -220,20 +220,20 @@ void sendFile(char* f, int connFd)
 // write n bytes to a descriptor ...
 ssize_t writen (int fd, const void *vptr, size_t n)
 {
-size_t nleft ;
-ssize_t nwritten ;
-const char *ptr ;
-ptr = vptr ;
-nleft = n ;
-while (nleft > 0) {
-if ( (nwritten = write(fd,ptr,nleft)) <= 0) {
-if (errno == EINTR)
-nwritten = 0 ; // and call write() again
-else
-return (-1) ;
-}
-nleft -= nwritten ;
-ptr += nwritten ;
-} ;
-return (n) ;
+	size_t nleft ;
+	ssize_t nwritten ;
+	const char *ptr ;
+	ptr = vptr ;
+	nleft = n ;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd,ptr,nleft)) <= 0) {
+			if (errno == EINTR)
+				nwritten = 0 ; // and call write() again
+			else
+			return (-1) ;
+		}
+		nleft -= nwritten ;
+		ptr += nwritten ;
+	} ;
+	return (n) ;
 } ;
